5387a20cfe296e9b02b4dfdb95fded304f45cc79
Meretskiy <A-Meretskiy@mail.ru>
2020-10-06 22:07:23 +0300

    Created client loader

--- cloud-server/src/main/java/com/meretskiy/cloud/storage/server/Main.java
+++ cloud-server/src/main/java/com/meretskiy/cloud/storage/server/Main.java
@@ -1,8 +1,10 @@
 package com.meretskiy.cloud.storage.server;
 
+import com.meretskiy.cloud.storage.common.Library;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Library lib = new Library();
     }
 }
